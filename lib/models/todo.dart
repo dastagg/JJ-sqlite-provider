f537293cf2fc1b207c4b6db5cca74c8186340959
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-22 21:34:08 -0400

    End of Video 2.
    One change: Instead of manually overriding == and hashcode, installed Equatable package and set that up.

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,3 +1,7 @@
+import 'package:equatable/equatable.dart';
+
+const String todoTable = 'todo';
+
 class TodoFields {
   static const String username = 'username';
   static const String title = 'title';
@@ -6,7 +10,7 @@ class TodoFields {
   static final List<String> allFields = [username, title, done, created];
 }
 
-class Todo {
+class Todo extends Equatable {
   final String username;
   final String title;
   bool done;
@@ -18,4 +22,31 @@ class Todo {
     this.done = false,
     required this.created,
   });
+
+  Map<String, Object?> toJson() => {
+        TodoFields.username: username,
+        TodoFields.title: title,
+        TodoFields.done: done ? 1 : 0,
+        TodoFields.created: created.toIso8601String(),
+      };
+
+  static Todo fromJson(Map<String, Object?> json) => Todo(
+        username: json[TodoFields.username] as String,
+        title: json[TodoFields.title] as String,
+        done: json[TodoFields.done] == 1 ? true : false,
+        created: DateTime.parse(json[TodoFields.created] as String),
+      );
+
+  @override
+  List<Object> get props => [username, title, done, created];
+// @override
+// bool operator ==(covariant Todo todo) {
+//   return (this.username == todo.username) &&
+//       (this.title.toUpperCase().compareTo(todo.title.toUpperCase()) == 0);
+// }
+//
+// @override
+// int get hashcode {
+//   return hashValues(username, title);
+// }
 }
